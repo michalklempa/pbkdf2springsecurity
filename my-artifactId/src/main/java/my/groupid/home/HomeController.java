857ce1ad35a25425fd9e10f95ce20beb5e5cf08c
Michal Klempa <mkl@eea.sk>
2015-02-06 15:00:13 +0100

    Adding code for showing existing password hash in the browser

--- my-artifactId/src/main/java/my/groupid/home/HomeController.java
+++ my-artifactId/src/main/java/my/groupid/home/HomeController.java
@@ -1,15 +1,25 @@
 package my.groupid.home;
 
 import java.security.Principal;
-
+import my.groupid.account.Account;
+import my.groupid.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Principal principal) {
-		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
-	}
+  @Autowired
+  private AccountRepository accountRepository;
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String index(Model model, Principal principal) {
+    if (principal != null) {
+      Account account = accountRepository.findByEmail(principal.getName());
+      model.addAttribute("passwordhash", account.getPassword());
+    }
+    return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+  }
 }
